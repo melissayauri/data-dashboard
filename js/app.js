6ee242cdea44db4c72e11ef17a15ae8919b49b58
marielacp <marielacp@outlook.com.pe>
2017-12-01 04:35:58 -0500

    reduciendo codigo filtros 2,3 y 4

--- js/app.js
+++ js/app.js
@@ -1,63 +1,39 @@
-/*Añadiendo 2da pregunta*/
-
-var containerFilter2 = document.getElementById('containerFilter2');
-var containerFilter3 = document.getElementById('containerFilter3');
-function showPercent(event) {
-    var contpercent = document.createElement('div');
-    contpercent.classList = 'filter2';
-    var percentxt = document.createElement('p');
-    percentxt.textContent = 'DROPOUT';
-    var percent = document.createElement('a');
+window.addEventListener('load', function() {
+// Creacion de contenedor principal por Melissa, llamando variable.
+  var selection = document.getElementById('selection');
+  selection.addEventListener('click', showPercent);
+  // Creando variables de contenidos
+  var containerFilter2 = document.getElementById('containerFilter2');
+  var containerFilter3 = document.getElementById('containerFilter3');
+  var containerFilter4 = document.getElementById('containerFilter4');
+  // Creando funcion filtros preguntas 2,3,4.
+  function showPercent(event) {
+    var selectiong = selection.value; // se iguala a opciones del contenedor.
+    var dataNew = selectiong.split('-'); // separamos convertimos nuevo array
+    var newArrayData = dataNew.splice(1, 2); // convertimos en array
+    var newArrayDataGeneration = newArrayData[0] + '-' + newArrayData[1];
+    var newArrayDataSede = dataNew.splice(0, 1);
+    var newDataGeneration = data[newArrayDataSede][newArrayDataGeneration];
+    console.log(newDataGeneration['students']);
     var countPerct = 0;
-    var active1 = data.AQP["2016-2"]["students"];
-    if (selection.value === "AQP-2016-2") {
-        for (var i = 0; i < active1.length; i++) {
-            if (active1[i]['active'] === false) {
-                countPerct++;
-                percent.textContent = ((countPerct * 100) / (active1.length)).toFixed(2) + '% ';
-                contpercent.appendChild(percent);
-                contpercent.appendChild(percentxt);
-                containerFilter2.appendChild(contpercent);
-                countPerct = '';
-            }
-        }
-    }
-    var active2 = data.AQP["2017-1"]["students"];
-    if (selection.value === "AQP-2017-1") {
-        for (var i = 0; i < active2.length; i++) {
-            if (active2[i]['active'] === false) {
-                countPerct++;
-                percent.textContent = ((countPerct * 100) / (active1.length)).toFixed(2) + '% ';
-                contpercent.appendChild(percent);
-                contpercent.appendChild(percentxt);
-                containerFilter2.appendChild(contpercent);
-                countPerct = '';
-
-            }
-        }
+    for (var i = 0; i < newDataGeneration['students'].length; i++) {
+      if (newDataGeneration['students'][i]['active'] === false) {
+        countPerct++;
+        var percent = ((countPerct * 100) / (newDataGeneration['students'].length)).toFixed(2) + '% ';
+        containerFilter2.innerHTML = percent + '</br>' + '% DROPOUT';
+      }
     }
     var countSprints = 0;
-    var contPoint = document.createElement('div');
-    contPoint.classList = 'filter2';
-    var contPoints = document.createElement('a');
-    var contPointTxt = document.createElement('p');
-    contPointTxt.textContent = 'STUDENTS THAT MEER THE TARGET';
-    contPointTxt.classList = 'filtertxt';
-    if (selection.value === "AQP-2016-2") {
-        for (var i = 0; i < active1.length; i++) {
-            for (var j = 0; j < active1[i]['sprints'].length; j++) {
-                if (active1[i]['sprints'][j]['score']['tech'] >= 1260 && active1[i]['sprints'][j]['score']['hse'] >= 840) {
-                    countSprints++;
-                    contPoints.textContent = countSprints;
-                    contPoint.appendChild(contPointTxt);
-                    contPoint.appendChild(contPoints)
-                    containerFilter3.appendChild(contPoint);
-
-                }
-            }
+    for (var i = 0; i < newDataGeneration['students'].length; i++) {
+      for (var j = 0; j < newDataGeneration['students'][i]['sprints'].length; j++) {
+        if (newDataGeneration['students'][i]['sprints'][j]['score']['tech'] >= 1260 && newDataGeneration['students'][i]['sprints'][j]['score']['hse'] >= 840) {
+          countSprints++;
+          containerFilter3.innerHTML = countSprints + '</br>' + 'STUDENTS THAT MEER THE TARGET';
+          var percentCountSprints = ((countSprints * 100) / (newDataGeneration['students'].length)).toFixed(2) + '% ';
+          containerFilter4.innerHTML = percentCountSprints + '</br>' + 'OF Total';
         }
+      }
     }
-}
+  }
 });
-
 
